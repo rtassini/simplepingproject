ac07f10b03e980e3436dc5f4486b08a7471248ad
Rodrigo Tassini <rodrigo.tassini@gmail.com>
2017-08-20 15:45:35 -0300

    Initial commit

---
+++ src/main/java/com/simplepingproject/runnable/IcmpPing.java
@@ -0,0 +1,49 @@
+package com.simplepingproject.runnable;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class IcmpPing implements Runnable {
+
+	private static final Logger logger = LoggerFactory.getLogger(IcmpPing.class);
+	
+	private String host = "";
+	
+	public IcmpPing(String host) {
+		this.host = host;
+	}
+	
+	@Override
+	public void run() {
+		ping();
+
+	}
+	
+	
+	public void ping() {
+		try{
+	        String strCommand = "";
+	        logger.info("My OS :" + System.getProperty("os.name"));
+	        if(System.getProperty("os.name").startsWith("Windows")) {
+	            // construct command for Windows Operating system
+	            strCommand = "ping -n 1 " + this.host;
+	        } else {
+	            // construct command for Linux and OSX
+	            strCommand = "ping -c 1 " + this.host;
+	        }
+	        System.out.println("Command: " + strCommand);
+	        // Execute the command constructed
+	        Process myProcess = Runtime.getRuntime().exec(strCommand);
+	        myProcess.waitFor();
+	        if(myProcess.exitValue() == 0) {
+	        	logger.info("true");
+	        } else {
+	        	logger.info("false");
+	        }
+	    } catch( Exception e ) {
+	        e.printStackTrace();
+	        logger.info("false");
+	    }
+	}
+
+}
